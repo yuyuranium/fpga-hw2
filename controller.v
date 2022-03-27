37a09ef23c17062ca5c1b71bb7d604f92670fd87
WillyChennnn <willychen1210@gmail.com>
2022-03-27 22:27:40 +0800

    delete the AND gate in controller

--- controller.v
+++ controller.v
@@ -1,8 +1,6 @@
 module controller (clk, rst, SW,
-                   rgb_pwm, on_pwm, 
                    color, brightness,
                    brtns_timeout,
-                   led4, led5
                   );
 
 parameter PURPLE = 24'h7F1FFF;
@@ -13,10 +11,7 @@ parameter CRIMSON = 24'hFF00FF;
 
 input clk, rst;
 input [1:0] SW;
-input [2:0] rgb_pwm;
-input on_pwm;
 input brtns_timeout;
-output [2:0] led4, led5;
 output reg [23:0] color;
 output  [4:0] brightness;
 
@@ -26,11 +21,6 @@ reg [4:0] brtns_cnt, nbrtns_cnt;
 reg cnt_mode;
 wire timeout_cnt_done;
 
-
-assign led4 = rgb_pwm;
-assign led5[2] = rgb_pwm[2] & on_pwm;
-assign led5[1] = rgb_pwm[1] & on_pwm;
-assign led5[0] = rgb_pwm[0] & on_pwm;
 assign brightness = brtns_cnt;
 assign timeout_cnt_done = &timeout_cnt;
 
@@ -53,57 +43,6 @@ always@(posedge brtns_timeout or posedge rst)begin
         timeout_cnt <= timeout_cnt + 10'd1;
     end
 end
-/*always@(*)begin   
-    if(brtns_timeout)begin
-        if(timeout_cnt_done)begin
-            ntimeout_cnt = 10'd0;
-        end
-        else begin
-            ntimeout_cnt = timeout_cnt + 10'd1;
-        end
-    end
-    else begin
-        ntimeout_cnt = timeout_cnt;
-    end
-end*/
-
-/*always@(posedge clk or posedge rst)begin
-    if(rst)begin
-        brtns_cnt <= 5'd0;
-    end
-    else begin
-        brtns_cnt <= nbrtns_cnt;
-    end
-end
-always@(*)begin
-    if(timeout_cnt_done)begin
-        if(cnt_mode)begin
-            nbrtns_cnt = brtns_cnt - 5'd1;
-        end
-        else begin
-            nbrtns_cnt = brtns_cnt + 5'd1;
-        end
-    end
-    else begin
-        nbrtns_cnt = brtns_cnt;
-    end
-end
-
-always@(posedge clk or posedge rst)begin
-    if(rst)begin
-        cnt_mode <= 1'b1;
-    end
-    else if(timeout_cnt == 10'd1022)begin
-        if( &brtns_cnt || ~|brtns_cnt )begin
-            cnt_mode <= !cnt_mode;
-        end
-        else begin
-        end
-    end
-    else begin
-    end
-end
-*/
 
 always@(posedge brtns_timeout or posedge rst)begin
     if(rst)begin
